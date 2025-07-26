0fb5d16d5201d08f5b2f72c50d0c9aea391b9817
Tejaswini <chepurisaitejaswini@gmail.com>
2025-07-26 21:22:52 +0530

    Add project files.

---
+++ SelectIn/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SelectIn.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
